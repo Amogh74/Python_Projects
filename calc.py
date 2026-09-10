f109a87f574d0cb2e667f278e8455e727852f4ac
Amogh Indikar <indikaramogh@gmail.com>
2026-09-10 13:46:23 +0530

    Calculator using python
    Using recursion , functions to make a working calculator

---
+++ calc.py
@@ -0,0 +1,49 @@
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+def add(n1, n2):
+    return n1 + n2
+def sub(n1,n2):
+    return n1-n2
+def mul(n1,n2):
+    return n1*n2
+def div(n1,n2):
+    return n1/n2
+
+operations = {
+    "+" : add,
+    "-" : sub,
+    "*" : mul,
+    "/" : div,
+}
+def calculator():
+    Should_accumulate = True
+    n1 = float(input("Enter The First Number :"))
+    while Should_accumulate:
+        n2 = float(input("Enter The Second  Number :"))
+        op = input("What Operation You want to perform ?  '+','-','*','/' :")
+        result = operations[op](n1, n2)
+        print(f"{n1} {op} {n2} = {result}")
+        choice = input(f"Type 'y' to continue with {result} , type 'n' for new calculation ").lower()
+
+        if choice =="y":
+            n1 = result
+        else:
+            Should_accumulate =False
+            print("\n"*20)
+            calculator()#recursion, can use 2 while loop instead of this but this is better
+calculator()
